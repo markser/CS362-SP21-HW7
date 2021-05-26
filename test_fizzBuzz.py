d39c2f3f2791043a0280bc47f26bc4664a6fb425
markser <serm@oregonstate.edu>
2021-05-26 09:27:18 -0700

    Wrote first test for fizzbuzz, checks output for value 1

---
+++ test_fizzBuzz.py
@@ -0,0 +1,22 @@
+import io
+from io import StringIO
+import sys
+import unittest
+from unittest.mock import patch
+import pytest
+import fizzBuzz
+
+# run with python3 command 
+# python3 testPalindrome.py.py
+
+class TestCalculator(unittest.TestCase):
+    @patch('builtins.input', side_effect=['1'])
+    def test_number_one(self,mock_input):
+        # with unittest.mock.patch('volumeOfCube.retrieve_input', return_value=-1):
+        capturedOutput = io.StringIO()                  # Create StringIO object
+        sys.stdout = capturedOutput                     #  and redirect stdout.
+        fizzBuzz.fizzBuzz()                                     # Call function.
+        sys.stdout = sys.__stdout__                     # Reset redirect.
+        expected = ['1', '']
+        actual = capturedOutput.getvalue().split('\n')
+        self.assertEqual(actual, expected)
